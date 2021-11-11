2ea1310f7400e166339679544e635a6606ed611d
Adam <adamknight1992@gmail.com>
2021-11-11 12:58:41 +0000

    Tweaks and stuff

--- Testing and Analysis/roi_analysis.py
+++ Testing and Analysis/roi_analysis.py
@@ -1,6 +1,7 @@
 import numpy
 import math
 import os
+import scipy.optimize
 
 def arc_wedge(dist_from_centre, column, centre_point, max_num_radii, max_num_wedges, radius, wedge):
     # finds the arc in which the pixel lies
@@ -19,13 +20,15 @@ def arc_wedge(dist_from_centre, column, centre_point, max_num_radii, max_num_wed
     return selected_arc, selected_wedge
 
 def roi_assign(xPx, yPx, centre_point, radius, wedge, max_num_radii, max_num_wedges, outputArray):
+
     for row in range(xPx):
         for column in range(yPx):
             dist_from_centre = math.sqrt((row-centre_point[1])**2 + (column-centre_point[2])**2)
             # Pixel must lie within the largest semi-circle to be processed
             if dist_from_centre < radius[max_num_radii-1]:
                 arc, wedge = arc_wedge(row, column, centre_point, max_num_radii, max_num_wedges, radius, wedge)
-                outputArray[arc,wedge,1] = outputArray[arc,wedge,1] + image[row,column]
+                outputArray[arc][wedge] = outputArray[arc][wedge] + image[row][column]
+
     return outputArray
 
 # angles generated here are defined as the boundaries between wedges
@@ -56,14 +59,48 @@ def generate_radii(max_num_radii, max_radius):
     return radius
 
 def read_image(image_path):
+
     with open(image_path, "r") as f:
         i = 0
+        image = []
         for line in f:
             li=line.strip()
             matrix_line = [float(i) for i in li.split("	")]
             image[i][0:] = (matrix_line)
             i = i+1
+
     return image
+
+def parse_file_name(file_path):
+
+    name_list = file_path.split("_")
+    for name in name_list:
+        if name == "SQA" or "SQE" or "IB" or "PFPE":
+            surface = name
+        elif name.isnumeric() == True:
+            delay = name
+        elif name == "Q11" or "Q12" or "Q13" or "Q14" or "Q15":
+            transition = name
+    
+    if 'surface' not in locals():
+        print ("No surface name found")
+        return
+
+    if 'delay' not in locals():
+        print ("No delay found in image name")
+        return
+
+    if 'transition' not in locals():
+        print ("No transition found in image name")
+        return
+
+    return surface, delay, transition
+
+def residuals(x, sin, sout):
+
+    resid = (sin*x) - sout
+
+    return resid
 
 probe_height = 0
 centre_point = []
@@ -84,6 +121,7 @@ timeStep = 0
 num_timepoints = (endTime - startTime) / timeStep
 
 folder_path = ""
+timePoint = ""
 
 image = numpy.zeros((xPx,yPx))
 outputArray = numpy.zeros((max_num_radii,max_num_wedges,2))
@@ -110,6 +148,21 @@ for root, dirs, files in os.walk(folder_path):
         # For each file, data are read into the image matrix
         image = read_image(file_path)
         # image goes to be processed, assigning the pixel intensity to the correct ROI
-        outputArray = roi_assign(xPx, yPx, centre_point, radius, wedge, max_num_radii, max_num_wedges, outputArray)
+        outputArray[:][:][timePoint][1] = roi_assign(xPx, yPx, centre_point, radius, wedge, max_num_radii, max_num_wedges, outputArray)
         # with open('Output Images/'+'sv_'+name+'.txt','wb') as f:
             #    numpy.savetxt(f, matrix, fmt='%.5e')
+
+# TOF generation and manipulation
+
+selected_wedge = 0
+sample_range_start = 0
+sample_range_end = 5
+
+sin = []
+sout = []
+
+fit = scipy.optimize.least_squares(residuals, 1)
+
+modifier = fit.x
+
+mod_sin = (sin*modifier) - sout
