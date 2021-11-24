fba15d8de01cc77348ecbdcb6ca5f4bb31d7f91f
Adam <adamknight1992@gmail.com>
2021-11-24 12:07:27 +0000

    getting there i guess

--- Testing and Analysis/roi_analysis.py
+++ Testing and Analysis/roi_analysis.py
@@ -80,13 +80,24 @@ def generate_radii(max_num_radii, max_radius):
 
 def read_image(image_path):
 
+    illegal_characters = ['','\n']
+    print (illegal_characters)
+
     with open(image_path, "r") as f:
         i = 0
         image = []
+        working_line = []
         for line in f:
-            li=line.strip()
-            matrix_line = [float(i) for i in li.split("	")]
-            image[i][0:] = (matrix_line)
+            #li=line.strip()
+            num = line.split(" ")
+            for element in num:
+                element = element.strip()
+                if element not in illegal_characters:
+                    working_line.append(element)
+            #matrix_line = [float(i) for i in li.split(" ")]
+            matrix_line = [float(i) for i in working_line]
+            #image[i][0:] = (matrix_line)
+            image.append(working_line)
             i = i+1
 
     return image
@@ -94,12 +105,15 @@ def read_image(image_path):
 def parse_file_name(file_path):
 
     name_list = file_path.split("_")
+    surface_list = ["SQA","SQE","IB","PFPE"]
+    transition_list = ["Q11","Q12","Q13","Q14","Q15"]
+
     for name in name_list:
-        if name == "SQA" or "SQE" or "IB" or "PFPE":
+        if name in surface_list:
             surface = name
         elif name.isnumeric() == True:
             delay = name
-        elif name == "Q11" or "Q12" or "Q13" or "Q14" or "Q15":
+        elif name in transition_list:
             transition = name
     
     if 'surface' not in locals():
@@ -116,36 +130,49 @@ def parse_file_name(file_path):
 
     return surface, delay, transition
 
+def simple_split(file_path):
+
+    file_path = file_path.split(".")
+    name_list = file_path[0].split("_")
+
+    for name in name_list:
+
+        if name.isnumeric():
+            delay =  name
+            return delay
+        else:
+            print ("meme")
+    print(name_list)
 def residuals(x, sin, sout):
 
     resid = (sin*x) - sout
 
     return resid
-"""
+
 probe_height = 0
-centre_point = []
-num_arcs = 0
-num_wedges = 0
+centre_point = [294, 210]
+num_arcs = 7
+num_wedges = 14
 
-xPx = 0
-yPx = 0
+xPx = 420
+yPx = 420
 
-max_num_radii = 0
-max_num_wedges = 0
+max_num_radii = 7
+max_num_wedges = 14
 max_radius = 0
 
-startTime = 0
-endTime = 0
-timeStep = 0
+startTime = 56
+endTime = 160
+timeStep = 1
 
 ############################# IMPORTANT #################################
-matrix[:,:,0,0] = smaller_matrix
+#matrix[:,:,0,0] = smaller_matrix
 ######################### END OF IMPORTANT ##############################
 
-num_timepoints = (endTime - startTime) / timeStep
+num_timepoints = int((endTime - startTime) / timeStep)
 
-folder_path = ""
-timePoint = ""
+folder_path = "/mnt/c/Users/adam/Documents/Code/Scattering Images/2021-11-24_100741/Blurred Images"
+#timePoint = ""
 
 image = numpy.zeros((xPx,yPx))
 outputArray = numpy.zeros((max_num_radii,max_num_wedges,num_timepoints,2))
@@ -168,14 +195,21 @@ for file in list:
 for root, dirs, files in os.walk(folder_path):
     for name in files:
         file_path = root + "/" + name
+        #surface, delay, transition = parse_file_name(file_path)
+        delay = simple_split(file_path)
+
         print (name)
         # For each file, data are read into the image matrix
         image = read_image(file_path)
         # image goes to be processed, assigning the pixel intensity to the correct ROI
-        outputArray[:][:][timePoint][1] = roi_assign(xPx, yPx, centre_point, radius, wedge, max_num_radii, max_num_wedges, outputArray)
+        print (delay, startTime)
+        outputArray[:][:][int(delay)-startTime][1] = roi_assign(xPx, yPx, centre_point, radius, wedge, max_num_radii, max_num_wedges, outputArray)
 
 # TOF generation and manipulation
 
+print(outputArray[:][:][100][1])
+
+"""
 selected_wedge = 0
 sample_range_start = 0
 sample_range_end = 5
